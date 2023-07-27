13005fe6dbc8ea262398de598fa548cc4b46bffa
amiralzd <123456amir78900@gmail.com>
2023-07-27 15:25:26 +0330

    responsive dashboard

--- js/script.js
+++ js/script.js
@@ -18,9 +18,29 @@ function handleMenu(){
         sidebarborder[i].classList.toggle ("w-[45px]")
     }
 
-    var sidebarfooter = document.getElementsByClassName("sidebar-footer");
-    for (let i = 0; i < sidebarfooter.length; i++) {
-        sidebarfooter[i].classList.toggle ("sidebarborderr")
-        
+    // var sidebarfooter = document.getElementsByClassName("sidebar-footer");
+    // for (let i = 0; i < sidebarfooter.length; i++) {
+    //     sidebarfooter[i].classList.toggle ("sidebarborderr")
+    // }
+    var sidebarfooter = document.getElementById("sidebar-footer");
+    sidebarfooter.classList.toggle("sidebarborderr")
+    
+
+    var sidebarwidth = document.getElementsByClassName("sidebar-width");
+    for (let i = 0; i < sidebarwidth.length; i++) {
+        sidebarwidth[i].classList.toggle ("w-[40%]")
     }
+
+    var sidebarflex = document.getElementsByClassName("stylell");
+    for (let i = 0; i < sidebarflex.length; i++) {
+        sidebarflex[i].classList.toggle ("stylel")
+    }
+}
+
+
+
+
+function handleMenu2(){
+    var sidebardisplay = document.getElementById("sidebar-card");
+    sidebardisplay.classList.toggle ("sidebarShow");
 }
